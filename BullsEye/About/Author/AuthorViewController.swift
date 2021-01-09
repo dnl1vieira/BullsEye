fe9533972f9d14fd2e437d81cc3343ed67b7d8e7
Daniel Vieira <danielvieira@Daniels-MacBook-Pro.local>
2021-01-09 16:36:30 -0300

    Finish Section 6 and 7

---
+++ BullsEye/About/Author/AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Daniel Vieira on 09/01/21.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backToAbout(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     var roundValue: Int = 0
     var scoreValue: Int = 0
     var titleValue: Int = 0
+    var scoreRound: Int = 0
     @IBOutlet weak var lblRound: UILabel!
     @IBOutlet weak var lblScore: UILabel!
     @IBOutlet weak var lblTitleAndRandomNumber: UILabel!
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stylingSlider()
         resetGame()
     }
 
@@ -29,14 +31,18 @@ class ViewController: UIViewController {
         let value = Int(sliderNumber.value)
         
         if value == titleValue {
+            fillScore(value: value)
+            showAlert(title: checkLabelsToShowInAlert(value: value), message: "You're right" + "\n You scored: " + String(scoreRound), actionTitle: "Continue")
+            newRound()
             titleValue = Int.random(in:0..<100)
             lblTitleAndRandomNumber.text = "\(txtTitle) \(titleValue)"
-            newRound()
-            rightHitScore()
-            showAlert(title: "Amazing!", message: "You're right", actionTitle: "Continue")
+
         }else{
+            fillScore(value: value)
+            showAlert(title: checkLabelsToShowInAlert(value: value), message: "You Score: " + String(scoreRound) + " \n because you selected: " + String(value) + "\n and the right value is: " + String(titleValue), actionTitle: "Try again")
             newRound()
-            showAlert(title: "Ops!", message: "You're wrong \n The diference was: " + String(calculateDifference(value: value)) + " \n and you selected: " + String(value), actionTitle: "Try again")
+            
+            
         }
         
     }
@@ -44,10 +50,6 @@ class ViewController: UIViewController {
     @IBAction func btnResetGame(_ sender: Any) {
         showAlert(title: "Restart Game", message: "Your game will be restarted", actionTitle: "Thanks!")
         resetGame()
-    }
-    
-    @IBAction func btnInformationOfTheGame(_ sender: Any) {
-        showAlert(title: "That's my game, Welcome", message: "I don't know what enter here, just play", actionTitle: "All right, I got it")
     }
     
     func showAlert(title: String, message: String, actionTitle: String){
@@ -62,8 +64,10 @@ class ViewController: UIViewController {
         lblRound.text = "\(txtRound) \(roundValue)"
     }
     
-    func rightHitScore(){
-        scoreValue += 1
+    func fillScore(value: Int){
+        let valueDifference = calculateDifference(value: value)
+        scoreRound = calculateTheScore(value: valueDifference)
+        scoreValue += scoreRound
         lblScore.text = "\(txtScore) \(scoreValue)"
     }
     
@@ -78,9 +82,58 @@ class ViewController: UIViewController {
         sliderNumber.minimumValue = 1
         sliderNumber.value = 50
     }
-    
     func calculateDifference(value: Int) -> Int{
-         return abs(value - titleValue)
+        return abs(value - titleValue)
+    }
+    
+    func calculateTheScore(value: Int) -> Int{
+        
+        let score: Int
+        if value == 0{
+            score = abs(100 - value) + 100
+            return score
+        }else if value == 1{
+            score = abs(100 - value) + 50
+            return score
+        }else{
+            score = abs(100 - value)
+            return score
+        }
+        
+    }
+ 
+    func checkLabelsToShowInAlert(value: Int) -> String{
+        let number = calculateDifference(value: value)
+        
+        if number == 0{
+            return "Amazing!"
+        }else if number < 5{
+            return "You Almost had it."
+        }else if number < 10 {
+            return "Pretty good."
+        }else{
+            return "Not even close..."
+        }
+        
+    }
+    
+    func stylingSlider(){
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        
+        sliderNumber.setThumbImage(thumbImageNormal, for: .normal)
+        sliderNumber.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        sliderNumber.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        sliderNumber.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+        
+        
     }
     
 }
